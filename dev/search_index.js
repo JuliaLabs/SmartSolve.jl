7004d6abaf6ed740a54b7791077a43d4308af52e
Documenter.jl <documenter@juliadocs.github.io>
2025-11-21 22:49:23 +0000

    build based on 20f4e6f

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#SmartSolve","page":"Home","title":"SmartSolve","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SmartSolve.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+[{"location":"#SmartSolve","page":"Home","title":"SmartSolve","text":"Documentation for SmartSolve.\n\n","category":"section"}]
 }
